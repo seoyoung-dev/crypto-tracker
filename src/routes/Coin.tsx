a3a9496410ac1785bc180cccee46c194646288fb
seoyoung-dev <seoyoung.dev@gmail.com>
2022-03-29 23:58:17 +0900

    connect API

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,13 @@
 import { useParams } from "react-router";
 
 interface RouteParams {
-  coinId: string;
+    coinId: string;
 }
 
 function Coin() {
-  const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+    const { coinId } = useParams<RouteParams>();
+    return <h1>Coin: {coinId}</h1>;
 }
 export default Coin;
+
+// react-router-dom v6부터는 제네릭을 지원하지 않음
